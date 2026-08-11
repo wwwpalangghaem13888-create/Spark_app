21dd317ffb7fb3f55658aab626c094ebf25457b6
wwwpalangghaem13888-create <www.palang.ghaem13888@gmail.com>
2026-08-11 20:57:27 +0330

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Spark Habit Tracker"
+include(":app")
